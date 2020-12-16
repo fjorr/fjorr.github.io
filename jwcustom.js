33e335de195ea9cc720893e76eeeed9cb7002680
Thor Ronér <thorroner@gmail.com>
2020-12-15 16:57:05 -0800

    Intial commit

---
+++ jwcustom.js
@@ -0,0 +1,16 @@
+
+    var caption = 0;
+
+    playerInstance.on('ready', function() {
+        const playerContainer = jwplayer().getContainer();
+        const ccButton = playerContainer.querySelector('.jw-button-container .jw-icon-cc');
+        jwplayer().setCurrentCaptions(caption);
+        duration = jwplayer().getDuration();
+
+        ccButton.addEventListener("click", function() {
+          caption = jwplayer().getCurrentCaptions();
+          caption = (caption == 0 ) ? 1 : 0;
+          jwplayer().setCurrentCaptions(caption);
+        });
+        
+    });
